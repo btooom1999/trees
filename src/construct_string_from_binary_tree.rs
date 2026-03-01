0defe7c2b6ac2288d4a36a78cb53729add3c2cc5
btooom1999 <btooom1999@gmail.com>
2026-03-01 17:45:09 +0700

    finished construct_string_from_binary_tree exercise

--- src/construct_string_from_binary_tree.rs
+++ src/construct_string_from_binary_tree.rs
@@ -47,54 +47,44 @@ fn vec_to_tree(nums: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
     Some(root)
 }
 
-fn helper(node: Option<Rc<RefCell<TreeNode>>>) -> Option<String> {
+fn helper(node: Option<Rc<RefCell<TreeNode>>>, vec: &mut Vec<u8>) {
     if let Some(node_rc) = node {
         let node = node_rc.borrow();
-        match (helper(node.right.clone()), helper(node.left.clone())) {
-            (Some(val2), Some(val1)) => {
-                Some(format!("({}{}{})", node.val, val1, val2))
+        vec.extend(node.val.to_string().as_bytes());
+
+        match (node.left.clone(), node.right.clone()) {
+            (Some(left), Some(right)) => {
+                vec.push(b'(');
+                helper(Some(left), vec);
+                vec.push(b')');
+                vec.push(b'(');
+                helper(Some(right), vec);
+                vec.push(b')');
             }
-            (Some(val), _) => {
-                Some(format!("({}(){})", node.val, val))
-            },
-            (_, Some(val)) => {
-                Some(format!("({}{})", node.val, val))
-            },
-            _ => {
-                Some(format!("({})", node.val))
-            },
+            (Some(node), _) => {
+                vec.push(b'(');
+                helper(Some(node), vec);
+                vec.push(b')');
+            }
+            (_, Some(node)) => {
+                vec.extend("()(".as_bytes());
+                helper(Some(node), vec);
+                vec.push(b')');
+            }
+            _ => {}
         }
-    } else {
-        None
     }
 }
 
 fn tree2str(root: Option<Rc<RefCell<TreeNode>>>) -> String {
-    if root.is_none() {
-        return String::new();
-    }
+    let mut vec = Vec::new();
+    helper(root, &mut vec);
 
-    let root = root.unwrap();
-    let node = root.borrow();
-    match (node.left.clone(), node.right.clone()) {
-       (Some(left), Some(right)) => {
-            let str1 = helper(Some(left.clone()));
-            let str2 = helper(Some(right.clone()));
-            format!("{}{}{}", node.val, str1.unwrap(), str2.unwrap())
-        }
-        (Some(n), _) => {
-            let str = helper(Some(n.clone()));
-            format!("{}{}", node.val, str.unwrap())
-        }
-        (_, Some(n)) => {
-            let str = helper(Some(n.clone()));
-            format!("{}(){}", node.val, str.unwrap())
-        }
-        _ => { format!("{}", node.val) }
-    }
+    String::from_utf8(vec).unwrap()
 }
 
 pub fn main() {
+    // let root = vec![Some(1), Some(2), None, Some(3)];
     let root = vec![Some(1), None, Some(2), None, Some(3), None, Some(4), None, Some(5), None, Some(6)];
     // let root = vec![Some(1), Some(2), Some(3), None, Some(5), Some(6), Some(7), Some(8), Some(9), None, Some(10), Some(11)];
     println!("{}", tree2str(vec_to_tree(root)));
